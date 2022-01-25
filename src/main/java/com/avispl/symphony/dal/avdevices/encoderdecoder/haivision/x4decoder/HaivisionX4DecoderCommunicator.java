cb2c55fe9d956305c99b4810a2c779124e3b4b6c
HieuLM-AVISPL <HM15015@avispl.com>
2022-01-25 16:01:16 +0700

    SYAL-702 feat: create draft adapter

---
+++ src/main/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/x4decoder/HaivisionX4DecoderCommunicator.java
@@ -0,0 +1,70 @@
+/*
+ *  * Copyright (c) 2022 AVI-SPL, Inc. All Rights Reserved.
+ */
+package com.avispl.symphony.dal.avdevices.encoderdecoder.haivision.x4decoder;
+
+import com.avispl.symphony.api.dal.control.Controller;
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.api.dal.dto.monitor.Statistics;
+import com.avispl.symphony.api.dal.monitor.Monitorable;
+import com.avispl.symphony.dal.communicator.RestCommunicator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An implementation of RestCommunicator to provide communication and interaction with Haivision X4 Encoders
+ * Supported features are:
+ * <p>
+ * Monitoring:
+ * <p>
+ * Controlling:
+ *
+ * @author Harry
+ * @since 1.0.0
+ */
+public class HaivisionX4DecoderCommunicator extends RestCommunicator implements Monitorable, Controller {
+	/**
+	 * This method is called by Symphony to control the properties in the device
+	 *
+	 * @param controllableProperty ControllableProperty instance
+	 * @throws Exception if unexpected the value
+	 */
+	@Override
+	public void controlProperty(ControllableProperty controllableProperty) throws Exception {
+
+	}
+
+	/**
+	 * This method is called by Symphony to control the properties in the device
+	 *
+	 * @param list the list ControllableProperty instance
+	 * @throws Exception if ControllableProperty list is empty
+	 */
+	@Override
+	public void controlProperties(List<ControllableProperty> list) throws Exception {
+
+	}
+
+	/**
+	 * This method is called by Symphony to get the list of statistics to be displayed
+	 *
+	 * @return List<Statistics> This return the list of statistics
+	 */
+	@Override
+	public List<Statistics> getMultipleStatistics() throws Exception {
+		ExtendedStatistics extendedStatistics = new ExtendedStatistics();
+		Map<String, String> stats = new HashMap<>();
+		extendedStatistics.setStatistics(stats);
+
+		return Collections.singletonList(extendedStatistics);
+	}
+
+	@Override
+	protected void authenticate() throws Exception {
+
+	}
+}
