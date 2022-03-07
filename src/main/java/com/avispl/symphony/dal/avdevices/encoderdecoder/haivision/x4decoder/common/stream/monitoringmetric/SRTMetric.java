ecc918f137821d75bbf20b6227a5e2954c02309d
HieuLM-AVISPL <HM15015@avispl.com>
2022-03-07 10:04:43 +0700

    SYAL-751 feat: Implement decoder control and its UT

---
+++ src/main/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/x4decoder/common/stream/monitoringmetric/SRTMetric.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 AVI-SPL, Inc. All Rights Reserved.
+ */
+
+package com.avispl.symphony.dal.avdevices.encoderdecoder.haivision.x4decoder.common.stream.monitoringmetric;
+
+/**
+ * Set of SRT monitoring metrics keys
+ *
+ * @author Harry
+ * @since 1.0
+ */
+public enum SRTMetric {
+
+	// Static metric
+	RECONNECTIONS("SRTReconnections"),
+	LOST_PACKETS("SRTLostPackets"),
+	SENT_ACKS("SRTSentAcks"),
+	SENT_NAKS("SRTSentNaks"),
+	DROPPED_PACKET("SRTSkippedPackets"),
+	PATH_MAX_BANDWIDTH("SRTPathMaxBandwidth"),
+	RTT("SRTRtt"),
+
+	// TODO: Can be Historical metric in next version
+	BUFFER("SRT Buffer"),
+	SRT_LATENCY("SRT Latency");
+
+	private final String name;
+
+	/**
+	 * Parameterized constructor
+	 *
+	 * @param name Name of Decoder monitoring metric
+	 */
+	SRTMetric(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * retrieve {@code {@link #name}}
+	 *
+	 * @return value of {@link #name}
+	 */
+	public String getName() {
+		return this.name;
+	}
+}
+
