887258a4efbe2ee5c41eb71a75a0f4f9d2ccf53c
maksym.rossiitsev <maksym.rossiitsev@avispl.com>
2025-03-17 13:24:58 +0200

    Restore url generation, current host loadbalancer fails with explicit port specification, so it's not an adapter side issue

--- src/main/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/x4decoder/HaivisionX4DecoderCommunicator.java
+++ src/main/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/x4decoder/HaivisionX4DecoderCommunicator.java
@@ -1,5 +1,5 @@
 /*
- *  * Copyright (c) 2022 AVI-SPL, Inc. All Rights Reserved.
+ *  * Copyright (c) 2022-2025 AVI-SPL, Inc. All Rights Reserved.
  */
 package com.avispl.symphony.dal.avdevices.encoderdecoder.haivision.x4decoder;
 
